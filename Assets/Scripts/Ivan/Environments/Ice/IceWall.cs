84b96afee3d8f99537c64d1a5e6d7d8ab3b2492e
Soutenance15 <soutenance15@proton.me>
2025-11-27 16:04:44 +0400

    Update IceWall

--- Assets/Scripts/Ivan/Environments/Ice/IceWall.cs
+++ Assets/Scripts/Ivan/Environments/Ice/IceWall.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class IceWall : EnvironmentGame
@@ -5,14 +6,24 @@ public class IceWall : EnvironmentGame
     public IceWallData iceWallData;
 
     public float dragOnIce = 45f;
-    public float speedMultiplier = 2f;
-    public bool killFireForm = true;
+    public float speed = 4f;
+    public float range = 10f;
+
+    float startX;
+
+    void Awake()
+    {
+        startX = transform.position.x;
+    }
 
     void OnCollisionEnter(Collision other)
     {
         PlayerIceSystem player = other.gameObject.GetComponent<PlayerIceSystem>();
         if (player != null)
+        {
             iceWallData.PlayerEnter(this);
+            StartCoroutine(MoveWall());
+        }
     }
 
     void OnCollisionExit(Collision other)
@@ -20,5 +31,23 @@ public class IceWall : EnvironmentGame
         PlayerIceSystem player = other.gameObject.GetComponent<PlayerIceSystem>();
         if (player != null)
             iceWallData.PlayerExit();
+    }
+
+    IEnumerator MoveWall()
+    {
+        float targetX = startX + range;
+
+        while (transform.position.x < targetX)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+            yield return null; // attendre la prochaine frame
+        }
+
+        while (transform.position.x > startX)
+        {
+            transform.Translate(-Vector3.right * speed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
