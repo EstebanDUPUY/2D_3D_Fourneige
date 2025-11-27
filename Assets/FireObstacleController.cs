1157a9c42b8c9aa24c676da9037e84e3a3a2f622
SaeZygaena <hugo.altiery@hotmail.fr>
2025-11-27 09:04:37 +0400

    Commit Geyser done

---
+++ Assets/FireObstacleController.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class FireObstacleController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float forceGeyser;
+    private bool inGeyser;
+    private bool inBodyGeyser;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (inGeyser)
+            rb.AddForce(0, forceGeyser, 0, ForceMode.Impulse);
+    }
+    public void GeyserAction(float force, bool isInGeyser, bool isInBody)
+    {
+        if (inBodyGeyser != isInBody && inGeyser) return;
+        
+        forceGeyser = force;
+        inGeyser = isInGeyser;
+        inBodyGeyser = isInBody;
+    }
+}
