8b38f4b8550e8e2c1e5373873582967316012497
Soutenance15 <soutenance15@proton.me>
2025-12-01 14:54:02 +0400

    Manage direction for surfur cave explosion

--- Assets/Scripts/Ivan/Player/PlayerDamageSystem.cs
+++ Assets/Scripts/Ivan/Player/PlayerDamageSystem.cs
@@ -19,8 +19,18 @@ public class PlayerDamageSystem : MonoBehaviour
     public void Explode()
     {
         Debug.Log("PlayerDamageSystem Explode Call");
-        rb.linearVelocity = (Vector3.right + Vector3.up * 0.6f).normalized * 13f;
+
+        Vector3 baseDir = (Vector3.right + Vector3.down * 0.6f).normalized;
+
+        // Vérifie si le joueur se déplace vers la gauche ou la droite
+        float directionSign = rb.linearVelocity.x < 0 ? -1f : 1f;
+
+        // On inverse seulement la partie horizontale du vecteur
+        Vector3 finalDir = new Vector3(baseDir.x * directionSign, baseDir.y, baseDir.z);
+
+        rb.linearVelocity = -finalDir * 13f;
         rb.linearDamping = 1.6f;
+
         Die?.Invoke();
     }
 }
