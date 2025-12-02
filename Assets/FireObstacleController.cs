a3d440119aa14182ccfe0d55c273cb4887bb301e
SaeZygaena <hugo.altiery@hotmail.fr>
2025-12-02 14:18:18 +0400

    Made_Brume

--- Assets/FireObstacleController.cs
+++ Assets/FireObstacleController.cs
@@ -11,22 +11,39 @@ public class FireObstacleController : MonoBehaviour
     private bool inGeyser;
     private bool inBodyGeyser;
 
-    private _FezPlayerController state;
+    private FezPlayerController state;
+    private Vector3 posPlayerOnScreen;
 
     [SerializeField] public VolumeProfile profil;
+    [SerializeField] private bool isBrumeActivated = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        state = GetComponent<_FezPlayerController>();
+        state = GetComponent<FezPlayerController>();
     }
 
-  
+    void Update()
+    {
+        posPlayerOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (isBrumeActivated && profil.TryGet(out Vignette vignette))
+        {
+            vignette.center.value = new Vector2(posPlayerOnScreen.x, posPlayerOnScreen.y);
+
+            if (state.currentState == FezPlayerController.States.Fire)
+                vignette.intensity.value = 0.2f;
+            else
+                vignette.intensity.value = 1f;
+        }
+    }
+
+
     public void OnChangeLight()
     {
-        if (profil.TryGet<Vignette>(out Vignette vignette))
+        if (profil.TryGet(out Vignette vignette))
         {
-            if (state.currentState == _FezPlayerController.States.Fire)
+            if (state.currentState == FezPlayerController.States.Fire)
                 vignette.intensity.value = 0.2f;
             else
                 vignette.intensity.value = 1f;
