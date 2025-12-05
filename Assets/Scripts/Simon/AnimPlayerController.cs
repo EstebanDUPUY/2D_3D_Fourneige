dff156fd86d38e33cc940212b3d8015c4c5a48fa
Symon91 <simleblay@gmail.com>
2025-12-05 12:18:10 +0400

    animations already

--- Assets/Scripts/Simon/AnimPlayerController.cs
+++ Assets/Scripts/Simon/AnimPlayerController.cs
@@ -5,9 +5,9 @@ public class AnimPlayerController : MonoBehaviour
     Animator anim;
     Rigidbody rb;
 
-    public bool isDashing;
-    public bool isWallJumping;
-    public bool isDead;
+    public bool IsDashing;
+    public bool IsWallJumping;
+    public bool IsDead;
 
     public bool dashTriggered;
     public bool deadTriggered;
@@ -23,11 +23,11 @@ public class AnimPlayerController : MonoBehaviour
         float moveSpeed = Mathf.Abs(rb.linearVelocity.x);
         anim.SetFloat("Speed", moveSpeed);
 
-        anim.SetBool("isJumping", rb.linearVelocity.y > 0);
-        anim.SetBool("isFalling", rb.linearVelocity.y < 0);
-        anim.SetBool("isDashing", isDashing);
-        anim.SetBool("isWallJumping", isWallJumping);
-        anim.SetBool("isDead", isDead);
+        anim.SetBool("IsJumping", rb.linearVelocity.y > 0);
+        anim.SetBool("IsFalling", rb.linearVelocity.y < 0);
+        anim.SetBool("IsDashing", IsDashing);
+        anim.SetBool("IsWallJumping", IsWallJumping);
+        anim.SetBool("IsDead", IsDead);
 
         if (dashTriggered)
         {            
