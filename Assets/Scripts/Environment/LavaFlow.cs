5500f5c686d6a946cf8a2f02d8db10059456c79b
Soutenance15 <soutenance15@proton.me>
2025-12-17 14:06:17 +0400

    Add Prefab

---
+++ Assets/Scripts/Environment/LavaFlow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LavaFlow : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerDamageSystem playerDamage = other.GetComponent<PlayerDamageSystem>();
+            playerDamage.DieNow(1f);
+        }
+    }
+}
